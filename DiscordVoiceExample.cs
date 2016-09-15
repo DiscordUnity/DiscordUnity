f52d5d7d40fcd5e7661353ad9515bf13e2981e09
BlindArcher <robbedecraemer@hotmail.com>
2016-09-16 01:54:16 +0200

    DiscordUnity is now up to date for latest unity version

--- DiscordVoiceExample.cs
+++ DiscordVoiceExample.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using DiscordUnity;
 using System;
+using System.Net;
 using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 
 public class Buffer<T>
 {
@@ -69,6 +72,7 @@ public class DiscordVoiceExample : MonoBehaviour
         source = GetComponent<AudioSource>();
         receivedClip = AudioClip.Create("ReceivedAudio", 9600, 1, 48000, false);
         source.clip = receivedClip;
+        ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
         client = new DiscordClient();
     }
 
@@ -241,5 +245,30 @@ public class DiscordVoiceExample : MonoBehaviour
     {
         Debug.Log("Received packet: " + e.unitypacket.Length);
         buffer[e.unitypacket.Length] = e.unitypacket;
+    }
+
+    public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+    {
+        bool isOk = true;
+        // If there are errors in the certificate chain, look at each error to determine the cause.
+        if (sslPolicyErrors != SslPolicyErrors.None)
+        {
+            for (int i = 0; i < chain.ChainStatus.Length; i++)
+            {
+                if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown)
+                {
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                    bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                    if (!chainIsValid)
+                    {
+                        isOk = false;
+                    }
+                }
+            }
+        }
+        return isOk;
     }
 }
